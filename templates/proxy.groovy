a5affd9ca9d4fdbb0ea3550d939fb29bd90391b9
Simon Hafner <shafner@oaganalytics.com>
2016-02-01 20:10:44 +0800

    prefixed all variable names

--- templates/proxy.groovy
+++ templates/proxy.groovy
@@ -2,10 +2,10 @@ import jenkins.model.*
 
 def instance = Jenkins.getInstance()
 
-final String name = "{{proxy_host}}"
-final int port = {{proxy_port}}
-final String userName = "{{proxy_login}}"
-final String password = "{{proxy_password}}"
+final String name = "{{jenkins_proxy_host}}"
+final int port = {{jenkins_proxy_port}}
+final String userName = "{{jenkins_proxy_login}}"
+final String password = "{{jenkins_proxy_password}}"
 final String noProxyHost = "localhost"
 
 final def pc = new hudson.ProxyConfiguration(name, port, userName, password, noProxyHost)
